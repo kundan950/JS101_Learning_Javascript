c7b1fe05673c5eea17f43f410fdeb4c1ffc42aff
kundan950 <royalkundan950@gmail.com>
2023-08-30 20:37:12 +0530

    Pushing Introduction to Javascript Problems

---
+++ Introduction_to_Javascript/p1.js
@@ -0,0 +1,2 @@
+var name ="masai school";
+console.log(name)
---
+++ Introduction_to_Javascript/p2.js
@@ -0,0 +1,13 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// // Print the value stored in the variable name
+
+var name = "kundan singh";
+console.log(name)
+var father_name = "uchhab sing";
+console.log (father_name );
+var mother_name = ("santhos kanwar");
+console.log(mother_name)
